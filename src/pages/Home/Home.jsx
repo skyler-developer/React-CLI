77f4ab0ec93922d7a2fa22b435592ae3ce5a6702
skyler <515266316@qq.com>
2024-07-17 13:59:50 +0800

    feat: 增加preload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Home.less";
-
+import Message from "../Message/Message";
 function Home() {
     // throw new Error("error");
-    return <div className="title">Homeaaaaaaaaaa</div>;
+    return (
+        <div className="title">
+            Homeaaaaaaaaaa
+            <Message />
+        </div>
+    );
 }
 
 export default Home;
---
+++ src/pages/Message/Message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Message() {
+    return <div>message</div>;
+}
+
+export default Message;
