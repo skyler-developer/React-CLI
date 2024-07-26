2aa21df250f165a4b0624e6b4e2d30b525841f1a
skyler <skylerhaut@foxmail.com>
2024-07-26 18:06:21 +0800

    feat: 取消开发环境下的PWA

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -174,12 +174,13 @@ module.exports = {
         }),
 
         // pwa配置插件
-        new WorkboxPlugin.GenerateSW({
-            // 这些选项帮助快速启用 ServiceWorkers
-            // 不允许遗留任何“旧的” ServiceWorkers
-            clientsClaim: true,
-            skipWaiting: true,
-        }),
+        isProduction &&
+            new WorkboxPlugin.GenerateSW({
+                // 这些选项帮助快速启用 ServiceWorkers
+                // 不允许遗留任何“旧的” ServiceWorkers
+                clientsClaim: true,
+                skipWaiting: true,
+            }),
 
         isProduction &&
             new MiniCssExtractPlugin({
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-    return <div>Hello world!</div>;
+    return (
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#49d4fd",
+                fontSize: "30px",
+            }}>
+            <div>Hello world!</div>
+        </div>
+    );
 }
 
 export default App;
